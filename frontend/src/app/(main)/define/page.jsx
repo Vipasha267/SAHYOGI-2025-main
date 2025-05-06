59122b1eea0c0a92e7ebf3745eca9a153a7f7c50
Vipasha267 <vipashasingh267@gmail.com>
2025-05-06 12:16:16 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/(main)/define/page.jsx
+++ frontend/src/app/(main)/define/page.jsx
@@ -19,8 +19,14 @@ export default function DefinePage() {
   });
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="relative h-screen">
+      <img src="https://ofhsoupkitchen.org/wp-content/uploads/2024/01/how-to-help-others-2.jpg"
+        className="absolute inset-0 object-cover w-full h-full" alt="" />
+      <div className="relative h-screen bg-opacity-75 bg-lime-500/70 min-h-screen flex items-center justify-center ">
+      {/* <div className="relative h-screen flex items-center justify-center bg-opacity-75 bg-lime-500/70 "> */}
+
       <form onSubmit={formik.handleSubmit} className="flex gap-12">
+
         {/* NGO Box */}
         <div className="bg-white rounded-lg shadow-lg p-8 flex flex-col items-center w-64">
           <h2 className="text-xl font-bold mb-4">Are you an NGO?</h2>
@@ -41,9 +47,9 @@ export default function DefinePage() {
         <div className="bg-white rounded-lg shadow-lg p-8 flex flex-col items-center w-64">
           <h2 className="text-xl font-bold mb-4">Are you a Social Worker?</h2>
           <Link
-          
+
             type="button"
-            
+
             className="bg-lime-600 text-white px-6 py-2 rounded hover:bg-lime-700 transition"
             href="/socialworker-signup"
             onClick={() => {
@@ -55,6 +61,7 @@ export default function DefinePage() {
           </Link>
         </div>
       </form>
+    </div>
     </div>
   );
 }
